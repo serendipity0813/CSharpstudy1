f004902b3fc9d6d977872b1ee3d4c58b42dba12a
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-11-17 09:56:40 +0900

    Initial commit

---
+++ main/_Dispaly.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Note;
+
+namespace Note
+{
+    internal class Display
+    {
+        static void Main(string[] args) 
+        {
+            //Basic.Note1();
+            //Week2.Note2();
+            //Week3.Note3();
+            //Week4.Note4();
+
+        }
+    }
+}
